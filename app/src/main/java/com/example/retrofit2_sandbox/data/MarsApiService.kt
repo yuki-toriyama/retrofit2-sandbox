413b9975378fee3989d9f28c296261d4f1e6363c
Yuuki Toriyama <yuki_toriyama@waku-2.com>
2022-10-13 17:03:14 +0900

    update: MarsApiService.getPropertiesをsusupendに変更

--- app/src/main/java/com/example/retrofit2_sandbox/data/MarsApiService.kt
+++ app/src/main/java/com/example/retrofit2_sandbox/data/MarsApiService.kt
@@ -1,9 +1,8 @@
 package com.example.retrofit2_sandbox.data
 
-import retrofit2.Call
 import retrofit2.http.GET
 
 interface MarsApiService {
     @GET("realestate")
-    fun getProperties(): Call<List<MarsProperty>>
+    suspend fun getProperties(): List<MarsProperty>
 }
